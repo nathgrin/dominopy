1badec948536250d709aef458f60862ff6c201de
nathgrin <nathgrin@gmail.com>
2022-11-16 23:30:42 +0100

    On branch main Changes to be committed: 	modified:   dominopy.py 	new file:   out.json 	modified:   tst.pdf 	new file:   tst.png 	new file:   tst_formula.png 	new file:   tst_graph.png 	new file:   tst_table.png

--- dominopy.py
+++ dominopy.py
@@ -1,37 +1,48 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 plt.rc('font',family='STIXGeneral',size=14) # STIX looks like latex
 plt.rc('mathtext',fontset='stix')
 # plt.rc('figure', figsize=(1.41421356237*6.,6.) )
 # plt.rc('figure.subplot', right=0.96464466094,top=0.95 )
 plt.rc('lines', linewidth=1.8,marker='None',markersize=8 )
-plt.rc('axes', linewidth=1.5,labelsize=24,prop_cycle=plt.cycler(color=('k','r','c','darkorange','steelblue','hotpink','gold','b','maroon','darkgreen')) )
+plt.rc('axes', linewidth=1.5,labelsize=12,prop_cycle=plt.cycler(color=('k','r','c','darkorange','steelblue','hotpink','gold','b','maroon','darkgreen')) )
 plt.rc(('xtick.major','ytick.major'), size=5.2,width=1.5)
 # plt.rc(('xtick.minor','ytick.minor'), size=3.2,width=1.5,visible=True)
-# plt.rc(('xtick','ytick'), labelsize=20, direction='in' )
+plt.rc(('xtick','ytick'), labelsize=12)#, direction='in' )
 # plt.rc(('xtick'), top=True,bottom=True ) # For some stupid reason you have to do these separately
 # plt.rc(('ytick'), left=True,right=True )
 # plt.rc('legend',numpoints=1,scatterpoints=1,labelspacing=0.2,fontsize=18,fancybox=True,handlelength=1.5,handletextpad=0.5)
 # plt.rc('savefig', dpi=150,format='pdf',bbox='tight' )
 # plt.rc('errorbar',capsize=3.)
+# plt.rc('text',usetex=True)
 
 # plt.rc('image',cmap='gist_rainbow')
 
 
 def format_graph(ax):
-    ax.spines['left'].set_position('zero')
-    ax.spines['right'].set_color('none')
-    ax.spines['right'].set_position('zero')
-    ax.spines['bottom'].set_position('zero')
-    ax.spines['top'].set_color('none')
+    # pass
+    # ax.spines['left'].set_position('zero')
+    # ax.spines['right'].set_color('none')
+    # ax.spines['right'].set_position('zero')
+    # ax.spines['bottom'].set_position('zero')
+    # ax.spines['top'].set_color('none')
     ax.spines['top'].set_position('zero')
 
     # remove the ticks from the top and right edges
     ax.xaxis.set_ticks_position('both')
     ax.yaxis.set_ticks_position('both')
-    
+
+def format_formulatable(ax):
+    ax.spines['left'].set_color('none')
+    ax.spines['right'].set_color('none')
+    ax.spines['bottom'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
 
 def draw_ax_frame(fig,ax):
     bbox = ax.get_tightbbox(fig.canvas.get_renderer())
@@ -43,15 +54,7 @@ def draw_ax_frame(fig,ax):
     fig.add_artist(plt.Rectangle((x0-xpad, y0-ypad), width+2*xpad, height+2*ypad, edgecolor='black', linewidth=2, fill=False))
 
 
-def main():
-    print("Hello World!")
-    
-    
-    def _func(x):
-        return x*x
-    xarr = np.linspace(-5,5,1000)
-    yarr = _func(xarr)
-    
+def make_pages(obj_list):
     
     fig,axarr = plt.subplots(4,5, sharex=False, sharey=False, figsize=(11.6929,8.26772)) # (8.26772,11.6929)
 
@@ -67,11 +70,16 @@ def main():
     
     for i,row in enumerate(axarr):
         for j,ax in enumerate(row):
-            ax.plot(xarr, yarr)
+            # ax.plot(xarr, yarr)
 
             format_graph(ax)
             draw_ax_frame(fig,ax)
-    
+            
+            print(ax.get_xlim())
+            print(ax.xaxis.get_major_ticks())
+            print([x.get_loc() for x in ax.xaxis.get_major_ticks()])
+            # ax.set_xticks([x for x in ax.xaxis.get_major_ticks() if float(x.get_loc()) != 0.], minor=True)
+            print('tickz',ax.get_xticks(),ax.get_yticks())
     
     while True:
         try:
@@ -81,6 +89,112 @@ def main():
             input("Close pdf and press enter to try again")
             
     plt.show()
+    
+    
+    
+def make_json_file(fname):
+    def _func(x):
+        return x*x
+    xarr = np.linspace(-5,5,1000)
+    yarr = _func(xarr)
+    
+    obj_list = []
+    
+    def tstfunc(x):
+        return x*x
+    
+    def new_object():
+        thedict = {}
+        # thedict['name'] = ""
+        thedict['formula'] = {'text': ""}
+        thedict['graph'] = {'xrange':[-5,5]}
+        thedict['table'] = {'range':[-5,-3,-1,0,1,3,5]}
+        thedict['func'] = ""
+        
+        return thedict
+        
+    for i in range(2):
+        obj = new_object()
+        obj['formula']['text'] = "$y=2x-%d$"%(i)
+        # obj['func'] = marshal.dumps(tstfunc.func_code)
+        obj_list.append(obj)
+    
+    print(obj_list)
+    with open(fname,'w') as outfile:
+        json.dump(obj_list,outfile, indent=4)
+
+def make_figures(loc,obj):
+    
+    name = obj.get('name', "tst")
+    
+    def _inifig():
+        fig = plt.figure(figsize=(2,2),frameon=True) # (8.26772,11.6929)
+        ax =  fig.add_axes((0, 0, 1, 1))#fig.add_subplot(111)
+        
+        fig.subplots_adjust(left=0,right=1.,bottom=0,top=1.,wspace=0.,hspace=0.)
+        return fig,ax
+    ## Formula
+    fig,ax = _inifig()
+    ax.text(0.5, 0.5, obj['formula'].get('text',""), horizontalalignment='center',verticalalignment='center', transform=ax.transAxes)
+    
+    format_formulatable(ax)
+    plt.savefig('%s_formula.png'%(name), bbox_inches='tight')
+    
+    ## Table
+    fig,ax = _inifig()
+    
+    tablecontent = [[float(x) for x in obj['table']['range']],[]]
+    
+    tablecontent[1] = [ 2*x+1 for x in tablecontent[0] ]
+    
+    the_table = ax.table( cellText = tablecontent , rowLabels=['$x$','$y$'], edges='open', loc='center')
+    
+    for j in range(2):
+        for i in range(len(tablecontent[0])):
+            cell = the_table[j,i]
+            
+            cell.loc = 'center'
+            cell.visible_edges=""
+            if j == 0:
+                cell.visible_edges += "B"
+            if i != len(tablecontent[0])-1:
+                cell.visible_edges += "R"
+    
+    format_formulatable(ax)
+    plt.savefig('%s_table.png'%(name), bbox_inches='tight')
+    
+    
+    ## Graph
+    fig,ax = _inifig()
+    
+    xarr = np.arange(obj['graph']['xrange'][0],obj['graph']['xrange'][1],100)
+    yarr = xarr*xarr
+    
+    ax.plot(xarr,yarr)
+    
+    format_graph(ax)
+    plt.savefig('%s_graph.png'%(name), bbox_inches='tight')
+    
+    
+    # plt.savefig('tst.png', bbox_inches='tight')
+    # plt.show()
+
+def main():
+    print("Hello World!")
+    loc = "linquad/"
+    fname = "out.json"
+
+    make_json_file(fname)
+    
+    with open(fname) as infile:
+        in_obj_list = json.load(infile)
+    for i in range(len(in_obj_list)):
+        obj = in_obj_list[i]
+        # obj['func'] = types.FunctionType(the_obj['func'], globals(), "some_func_name") 
+        
+        make_figures(loc,obj)
+        
+    
     
     
 if __name__ == "__main__":
